a2cdca05f7a6fd6c62c134d1b6638ee85458300d
Nikita <obrikash@gmail.com>
2024-10-18 01:19:51 +0300

    vector class(also color and point), basic image.ppm

---
+++ build/CMakeFiles/PathTracing.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PathTracing.dir/main.cpp.o"
+  "CMakeFiles/PathTracing.dir/main.cpp.o.d"
+  "PathTracing"
+  "PathTracing.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PathTracing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/PathTracing.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for PathTracing.
+# This may be replaced when dependencies are built.
